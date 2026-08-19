24b836c4c8222bbeaab60b778911ed42fd9f5765
howard0327 <75930465+howard0327@users.noreply.github.com>
2026-08-19 23:48:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-fetch("nosure_dp.txt")
-    .then(response=>{
-        if(!response.ok){
-            throw new Error("DP檔案讀取失敗");
-        }
-        return response.text();
-    })
-    .then(text=>{
-        console.log("DP檔案讀取成功");
-        console.log("檔案大小：",text.length);
-        console.log(text.slice(0,500));
-    })
-    .catch(error=>{
-        console.error(error);
-    });
+console.log("script.js有執行");
